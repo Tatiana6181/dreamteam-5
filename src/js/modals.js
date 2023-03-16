58f91739729334ec9ccf3cd12264df64afadc281
Levis Artur <120251909+Levis-Artur@users.noreply.github.com>
2023-03-16 16:24:24 +0200

    close modal

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,88 +1,114 @@
-!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+!(function (e) {
+  'function' != typeof e.matches &&
+    (e.matches =
+      e.msMatchesSelector ||
+      e.mozMatchesSelector ||
+      e.webkitMatchesSelector ||
+      function (e) {
+        for (
+          var t = this,
+            o = (t.document || t.ownerDocument).querySelectorAll(e),
+            n = 0;
+          o[n] && o[n] !== t;
 
+        )
+          ++n;
+        return Boolean(o[n]);
+      }),
+    'function' != typeof e.closest &&
+      (e.closest = function (e) {
+        for (var t = this; t && 1 === t.nodeType; ) {
+          if (t.matches(e)) return t;
+          t = t.parentNode;
+        }
+        return null;
+      });
+})(window.Element.prototype);
 
-document.addEventListener('DOMContentLoaded', function() {
-
-   /* Записываем в переменные массив элементов-кнопок и подложку.
+document.addEventListener('DOMContentLoaded', function () {
+  /* Записываем в переменные массив элементов-кнопок и подложку.
       Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
-   var modalButtons = document.querySelectorAll('.js-open-modal'),
-       overlay      = document.querySelector('.js-overlay-modal'),
-       closeButtons = document.querySelectorAll('.js-modal-close');
+  var modalButtons = document.querySelectorAll('.js-open-modal'),
+    overlay = document.querySelector('.js-overlay-modal'),
+    closeButtons = document.querySelectorAll('.js-modal-close');
 
-
-   /* Перебираем массив кнопок */
-   modalButtons.forEach(function(item){
-
-      /* Назначаем каждой кнопке обработчик клика */
-      item.addEventListener('click', function(e) {
-
-         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+  /* Перебираем массив кнопок */
+  modalButtons.forEach(function (item) {
+    /* Назначаем каждой кнопке обработчик клика */
+    item.addEventListener('click', function (e) {
+      /* Предотвращаем стандартное действие элемента. Так как кнопку разные
             люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
             Нужно подстраховаться. */
-         e.preventDefault();
+      e.preventDefault();
 
-         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+      /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
             и будем искать модальное окно с таким же атрибутом. */
-         var modalId = this.getAttribute('data-modal'),
-             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+      var modalId = this.getAttribute('data-modal'),
+        modalElem = document.querySelector(
+          '.modal[data-modal="' + modalId + '"]'
+        );
 
-
-         /* После того как нашли нужное модальное окно, добавим классы
+      /* После того как нашли нужное модальное окно, добавим классы
             подложке и окну чтобы показать их. */
-         modalElem.classList.add('active');
-         overlay.classList.add('active');
-      }); // end click
+      modalElem.classList.add('active');
+      overlay.classList.add('active');
+    }); // end click
+  }); // end foreach
 
-   }); // end foreach
+  closeButtons.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      var parentModal = this.closest('.modal');
 
-
-   closeButtons.forEach(function(item){
-
-      item.addEventListener('click', function(e) {
-         var parentModal = this.closest('.modal');
-
-         parentModal.classList.remove('active');
-         overlay.classList.remove('active');
-      });
-
-   }); // end foreach
-
-
-    document.body.addEventListener('keyup', function (e) {
-        var key = e.keyCode;
-
-        if (key == 27) {
-
-            document.querySelector('.modal.active').classList.remove('active');
-            document.querySelector('.overlay').classList.remove('active');
-        };
-    }, false);
-
-
-    overlay.addEventListener('click', function() {
-        document.querySelector('.modal.active').classList.remove('active');
-        this.classList.remove('active');
+      parentModal.classList.remove('active');
+      overlay.classList.remove('active');
     });
+  }); // end foreach
 
+  document.body.addEventListener(
+    'keyup',
+    function (e) {
+      var key = e.keyCode;
 
+      if (key == 27) {
+        document.querySelector('.modal.active').classList.remove('active');
+        document.querySelector('.overlay').classList.remove('active');
+      }
+    },
+    false
+  );
 
-
+  overlay.addEventListener('click', function () {
+    document.querySelector('.modal.active').classList.remove('active');
+    this.classList.remove('active');
+  });
 }); // end ready
 
 /* form validation */
 
-let input = document.querySelector(".input");
-let button = document.querySelector(".button");
-
+let input = document.querySelector('.input');
+let button = document.querySelector('.button');
 
 button.disabled = true;
 
-input.addEventListener("change", stateHandle);
+input.addEventListener('change', stateHandle);
 
 function stateHandle() {
-   if (document.querySelector(".input").value === "") {
-      button.disabled = true;
-   } else {
-      button.disabled = false;
-   }
+  if (document.querySelector('.input').value === '') {
+    button.disabled = true;
+  } else {
+    button.disabled = false;
+  }
 }
+
+// отримуємо модальні вікна з атрибутом data-modal
+const modal1 = document.querySelector('[data-modal="1"]');
+const modal3 = document.querySelector('[data-modal="3"]');
+
+// додаємо обробник подій для кліку на модальному вікні з атрибутом data-modal="3"
+modal3.addEventListener('click', function (event) {
+  // перевіряємо, чи маємо модальне вікно з атрибутом data-modal="1"
+  if (modal1) {
+    // закриваємо модальне вікно з атрибутом data-modal="1"
+    modal1.style.display = 'none';
+  }
+});
